4706c9cbcde351785fc3e9879386732e995da4ea
Vivekagent47 <vivek.agent47@gmail.com>
2024-12-13 14:08:10 +0530

    add sidebar and breadcrumb

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import BreadcrumbHeader from "@/components/BreadcrumbHeader";
 import DesktopSideBar from "@/components/sideBar";
 import { Separator } from "@/components/ui/separator";
 import React from "react";
@@ -6,13 +7,13 @@ function DashBoardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen">
       <DesktopSideBar />
-      <div className="flex flex-col flex-1 min-h-screen">
-        <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
-          Flower
+      <div className="flex min-h-screen flex-1 flex-col">
+        <header className="container flex h-[50px] items-center justify-between px-6 py-4">
+          <BreadcrumbHeader />
         </header>
         <Separator />
         <div className="overflow-auto">
-          <div className="flex-1 container py-4 text-accent-foreground">
+          <div className="container flex-1 py-4 text-accent-foreground">
             {children}
           </div>
         </div>
--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -6,7 +6,10 @@ import {
   Layers2Icon,
   ShieldCheckIcon,
 } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import Logo from "./Logo";
+import { buttonVariants } from "./ui/button";
 
 const routes = [
   {
@@ -15,30 +18,53 @@ const routes = [
     icon: HomeIcon,
   },
   {
-    label: "workflows",
+    label: "Workflows",
     href: "workflows",
     icon: Layers2Icon,
   },
   {
-    label: "credentials",
+    label: "Credentials",
     href: "credentials",
     icon: ShieldCheckIcon,
   },
   {
-    label: "billing",
+    label: "Billing",
     href: "billing",
     icon: CoinsIcon,
   },
 ];
 
 function DesktopSideBar() {
+  const pathName = usePathname();
+  const activeRoute =
+    routes.find(
+      (route) => route.href.length > 0 && pathName.includes(route.href),
+    ) || routes[0];
+
   return (
     <div className="relative hidden h-screen w-full min-w-[280px] max-w-[280px] border-separate overflow-hidden border-r-2 bg-primary/5 text-muted-foreground dark:bg-secondary/30 dark:text-foreground md:block">
       <div className="flex border-separate items-center justify-center gap-2 border-b-[1px] p-4">
         <Logo />
       </div>
 
-      <div className="flex flex-col p-2"></div>
+      <div className="p-2">TODO CREDITS</div>
+      <div className="flex flex-col p-2">
+        {routes.map((route) => (
+          <Link
+            key={route.href}
+            href={route.href}
+            className={buttonVariants({
+              variant:
+                activeRoute.href === route.href
+                  ? "sidebarActiveItem"
+                  : "sidebarItem",
+            })}
+          >
+            <route.icon size={20} />
+            {route.label}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
